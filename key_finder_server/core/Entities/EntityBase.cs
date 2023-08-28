4a3f3a66adfe7581e11235ca6c74cefc30b35994
Md. Abdullah-Al-Bakee <mail.bakee@gmail.com>
2023-08-28 08:42:46 -0500

    Set default datetime for entities

--- key_finder_server/core/Entities/EntityBase.cs
+++ key_finder_server/core/Entities/EntityBase.cs
@@ -6,7 +6,8 @@ public abstract class EntityBase
 {
     [Key]
     public long Id { get; set; }
-    public DateTime Created { get; set; }
-    public DateTime Updated { get; set; }
+
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime Updated { get; set; } = DateTime.UtcNow;
     public bool IsDeleted { get; set; }
 }
